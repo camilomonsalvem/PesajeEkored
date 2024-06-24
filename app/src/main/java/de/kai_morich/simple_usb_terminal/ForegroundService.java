1f91c72bb1f266f632fac8805e60bc26e4978694
camilomonsalvem <monsalve88mm@gmail.com>
2024-06-24 15:40:04 -0500

    Actualizacion buffer sin notificacion

--- app/src/main/java/de/kai_morich/simple_usb_terminal/ForegroundService.java
+++ app/src/main/java/de/kai_morich/simple_usb_terminal/ForegroundService.java
@@ -113,7 +113,7 @@ public class ForegroundService extends Service {
             isStarted = true;
         }
 
-        handler.post(updateNotificationTask);
+        //handler.post(updateNotificationTask);
         return START_STICKY;
     }
 
@@ -121,7 +121,7 @@ public class ForegroundService extends Service {
         createServiceNotificationChannel();
         Notification notification = new NotificationCompat.Builder(this, CHANNEL_ID)
                 .setContentTitle("Aplicación Pesaje Ekored")
-                .setContentText("Peso: " + obtenerDatos())
+                .setContentText("La aplicación se esta ejecutando...")
                 .setSmallIcon(R.drawable.ic_notification)
                 .build();
         startForeground(ONGOING_NOTIFICATION_ID, notification);
@@ -142,7 +142,7 @@ public class ForegroundService extends Service {
     private void updateNotification(String weightData) {
         Notification notification = new NotificationCompat.Builder(this, CHANNEL_ID)
                 .setContentTitle("Aplicación Pesaje Ekored")
-                .setContentText("Peso: " + weightData)
+                .setContentText("La aplicación se esta ejecutando...")
                 .setSmallIcon(R.drawable.ic_notification)
                 .build();
         notificationManager.notify(ONGOING_NOTIFICATION_ID, notification);
